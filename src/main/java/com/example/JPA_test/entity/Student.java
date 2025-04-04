19561181354627189802575b1184623fedde61f4
ivanjur03 <150960067+ivanjur03@users.noreply.github.com>
2025-04-04 09:40:12 +0200

    entity gotov

--- src/main/java/com/example/JPA_test/entity/Student.java
+++ src/main/java/com/example/JPA_test/entity/Student.java
@@ -1,4 +1,27 @@
 package com.example.JPA_test.entity;
 
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.sql.Date;
+
+@Entity
+@Data
 public class Student {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name="id")
+    public Integer id;
+
+    @Column(name="name")
+    public String name;
+    @Column(name="email")
+    public String email;
+    @Column(name="age")
+    public Integer age;
+    @Column(name="birthday")
+    public Date birthday;
+
+
 }
