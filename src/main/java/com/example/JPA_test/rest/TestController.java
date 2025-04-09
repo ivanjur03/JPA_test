8fbd74820b41a62b6987df2462c6211dd37d8582
Gitoasty <matej.miklin11@gmail.com>
2025-04-09 10:57:00 +0200

    dodani logovi u controller

--- src/main/java/com/example/JPA_test/rest/TestController.java
+++ src/main/java/com/example/JPA_test/rest/TestController.java
@@ -3,6 +3,8 @@ package com.example.JPA_test.rest;
 import com.example.JPA_test.entity.Student;
 import com.example.JPA_test.service.StudentService;
 import com.example.JPA_test.service.TestService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,6 +17,8 @@ import java.util.List;
 public class TestController {
 
     private TestService testService;
+
+    Logger logger = LoggerFactory.getLogger(TestController.class);
 
     @Autowired
     private StudentService studentService;
@@ -32,27 +36,32 @@ public class TestController {
     @PostMapping("/student/save")
     public ResponseEntity<Student> save(@RequestBody Student student) {
         studentService.save(student);
+        logger.info("Student saved successfully");
         return ResponseEntity.status(HttpStatus.CREATED).body(student);
     }
 
     @GetMapping("/student/all")
     public List<Student> findAll() {
+        logger.info("Find all students");
         return studentService.findAll();
     }
 
     @GetMapping("/student/{age}")
     public List<Student> findStudentsOld(@PathVariable int age) {
+        logger.info("Find students older than " + age);
         return studentService.studentsOlderThan(age);
     }
 
     @GetMapping("/student/ivan")
     public Integer findStudentsIvan() {
+        logger.info("Find named Ivan");
         return studentService.studentsNamedIvan();
     }
 
     @DeleteMapping("/student/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void delete(@PathVariable int id) {
+        logger.info("Delete student with id " + id);
         studentService.deleteById(id);
     }
 }
